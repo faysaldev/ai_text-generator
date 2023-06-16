b3389484789219abf318dcad081aec713f7c234f
Faysal Mridha <fmridha166@gmail.com>
2023-06-16 16:09:21 +0600

    image alt addd

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
       <div className="flex flex-col space-x-10 text-center items-center justify-center">
         <Image
+          alt="Image for logo"
           src={
             "https://i.ibb.co/Jn49Jkr/Screenshot-2023-06-10-171245-removebg-preview-1.png"
           }
